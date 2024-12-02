57b16d6f5e11231bb92a3c3ead11050d555de2b2
Gabriel Carvalho <gabriel.carvalho@grupoigd.com.br>
2024-12-02 14:24:12 -0300

    Feat: Adiciona os Hooks Personalizados

---
+++ src/hooks/useLazyAxios.tsx
@@ -0,0 +1,38 @@
+import apiClient from "@/services/axiosClient";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { useState } from "react";
+
+interface LazyAxiosReturn<T> {
+  data: T | null;
+  error: Error | null;
+  isLoading: boolean;
+}
+
+type LazyAxiosExecute<T> = (
+  config: AxiosRequestConfig
+) => Promise<AxiosResponse<T> | void>;
+
+export function useLazyAxios<T = any>(): [
+  LazyAxiosExecute<T>,
+  LazyAxiosReturn<T>
+] {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const execute: LazyAxiosExecute<T> = async (config) => {
+    setIsLoading(true);
+    try {
+      const response: AxiosResponse<T> = await apiClient(config);
+      setData(response.data);
+      setError(null);
+      return response;
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return [execute, { data, error, isLoading }];
+}
