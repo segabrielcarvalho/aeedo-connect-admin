51be5f09b3f986ec711e26a4bf872952b893d3bb
filipe <filipegrodriguesreal@gmail.com>
2024-12-05 11:06:22 -0300

    FEAT: tela orgaos

--- src/app/dashboard/orgaos/_/components/OrgaosTable.tsx
+++ src/app/dashboard/orgaos/_/components/OrgaosTable.tsx
@@ -1,130 +1,68 @@
-'use client'
-import { PaperClipIcon } from '@heroicons/react/20/solid'
+"use client";
 
-export default function OrgaosTable() {
-  return (
-    <>
-      <div className="px-4 sm:px-0">
-        <h3 className="text-base/7 font-semibold text-gray-900">Applicant Information</h3>
-        <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">Personal details and application.</p>
-      </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Full name</dt>
-            <dd className="mt-1 flex text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="grow">Margot Foster</span>
-              <span className="ml-4 shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Application for</dt>
-            <dd className="mt-1 flex text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="grow">Backend Developer</span>
-              <span className="ml-4 shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Email address</dt>
-            <dd className="mt-1 flex text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="grow">margotfoster@example.com</span>
-              <span className="ml-4 shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Salary expectation</dt>
-            <dd className="mt-1 flex text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="grow">$120,000</span>
-              <span className="ml-4 shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">About</dt>
-            <dd className="mt-1 flex text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <span className="grow">
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-                qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure
-                nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-              </span>
-              <span className="ml-4 shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Attachments</dt>
-            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm/6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon aria-hidden="true" className="size-5 shrink-0 text-gray-400" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                      <span className="shrink-0 text-gray-400">2.4mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex shrink-0 space-x-4">
-                    <button
-                      type="button"
-                      className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Update
-                    </button>
-                    <span aria-hidden="true" className="text-gray-200">
-                      |
-                    </span>
-                    <button type="button" className="rounded-md bg-white font-medium text-gray-900 hover:text-gray-800">
-                      Remove
-                    </button>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm/6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon aria-hidden="true" className="size-5 shrink-0 text-gray-400" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                      <span className="shrink-0 text-gray-400">4.5mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex shrink-0 space-x-4">
-                    <button
-                      type="button"
-                      className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Update
-                    </button>
-                    <span aria-hidden="true" className="text-gray-200">
-                      |
-                    </span>
-                    <button type="button" className="rounded-md bg-white font-medium text-gray-900 hover:text-gray-800">
-                      Remove
-                    </button>
-                  </div>
-                </li>
-              </ul>
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </>
-  )
+import Button from "@/components/Button";
+import routes from "@/routes";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import SectionHeading from "../../../../../components/SectionHeading";
+import SelectableButton from "../../../../../components/SelectableButton";
+import { organIcons } from "../../../users/create/page";
+
+interface OrganSectionProps {
+  userOrgans: string[];
 }
 
+const OrgaosTable: React.FC<OrganSectionProps> = ({ userOrgans }) => {
+
+  const organsList = Object.keys(organIcons);
+
+  const [selectedOrgans, setSelectedOrgans] = useState<string[]>(userOrgans);
+
+  const router = useRouter();
+
+  const toggleOrganSelection = (organ: string) => {
+    setSelectedOrgans((prev) =>
+      prev.includes(organ) ? prev.filter((o) => o !== organ) : [...prev, organ]
+    );
+  };
+
+  return (
+    <div>
+      <form className="space-y-12 w-full">
+        <SectionHeading title="Órgãos Associados" />
+        <div className="grid sm:grid-cols-3 2xl:grid-cols-7 gap-4">
+          {organsList.map((organ) => (
+            <SelectableButton
+              key={organ}
+              isSelected={selectedOrgans.includes(organ)}
+              label={organ}
+              onClick={() => toggleOrganSelection(organ)}
+              icon={organIcons[organ as keyof typeof organIcons]}
+            />
+          ))}
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-3">
+          <Button
+            variant="outline"
+            color="primary"
+            type="button"
+            className="rounded-md px-5"
+            onClick={() => router.push(routes.dashboard.users.path)}
+          >
+            Cancelar
+          </Button>
+          <Button
+            variant="solid"
+            color="secondary"
+            className="rounded-md px-12"
+            type="submit"
+          >
+            Criar
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default OrgaosTable;
